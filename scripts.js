a08b1eefa34b307c73141e5ebe61c10c22e7431d
CUHRISZ <chris3cabero@gmail.com>
2022-10-09 23:33:40 +0000

    added AJAX code and added functions

--- scripts.js
+++ scripts.js
@@ -1,4 +1,29 @@
 
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// provides code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.querySelector(whereTo).innerHTML = this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+			}
+		
+	} // end ajax.onreadystatechange function
+
+	// initiate request and wait for response
+	ajax.send();
+
+}
+
+
 
 window.onload = function() {
     
@@ -24,7 +49,9 @@ window.onload = function() {
     this .classList.toggle("show");
   }  
 
-  
+  loadFileInto("cakeingredients.html", "#cakeingredients ul");
+  loadFileInto("frostingingredients.html", "#frostingingredients ul");
+  loadFileInto("directions.html", "#directions ol");
 }
 
 // classlist.toggle -> toggles class
